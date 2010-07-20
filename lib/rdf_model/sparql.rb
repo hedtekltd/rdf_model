474950e08699842a356b651c348cfbde1ae94c98
David Workman <workmad3@gmail.com>
2010-07-20 16:58:14 +0100

    Added basic query features for sparql

--- lib/rdf_model/sparql.rb
+++ lib/rdf_model/sparql.rb
@@ -7,5 +7,22 @@ module ::RdfModel::Sparql
     def connection
       ::RdfModel::TRIPLESTORE_CONFIG['connection-pool'].connection
     end
+
+    def find_by_sparql(query)
+      connection.select(query)
+    end
+
+    def find_by_uri(uri)
+      self.new(self.find_by_sparql("SELECT ?p ?o WHERE { #{uri} ?p ?o }"))
+    end
+
+    def find_by_id(id)
+      find_by_uri("#{id_prefix}:#{id}")
+    end
+
+    def id_prefix(prefix = nil)
+      @id_prefix = prefix if prefix
+      return @id_prefix
+    end
   end
 end
--- spec/rdf_model/sparql_spec.rb
+++ spec/rdf_model/sparql_spec.rb
@@ -3,17 +3,46 @@ require 'spec_helper'
 describe RdfModel::Sparql do
   before(:each) do
     RdfModel::TRIPLESTORE_CONFIG.load_rails_config_file(File.join(File.dirname(__FILE__), "..", "config", "triplestore.yml"), "test")
-    ::FourStore::Store.stub!(:new).and_return(mock(::FourStore::Store))
+    ::FourStore::Store.stub!(:new).and_return(@store = mock(::FourStore::Store))
+  end
+
+  def test_class
+    Class.new do
+      include RdfModel::Sparql
+    end
   end
 
   it "should re-use the same connection for multiple classes" do
     ::FourStore::Store.should_receive(:new).once().with("test-uri")
-    c = Class.new do
-      include RdfModel::Sparql
-    end
-    c2 = Class.new do
-      include RdfModel::Sparql
-    end
+    c = test_class
+    c2 = test_class
     c.connection.should == c2.connection
+  end
+
+  it "should allow queries to be made on the connection" do
+    c = test_class
+    @store.should_receive(:select).with("SELECT * WHERE { ?s ?o ?p }")
+    c.find_by_sparql("SELECT * WHERE { ?s ?o ?p }")
+  end
+
+  it "should create an object when using find_by_uri" do
+    c = test_class
+    @store.should_receive(:select).with("SELECT ?p ?o WHERE { http://test.host/ ?p ?o }").and_return({"p" => "blah", "o" => "test"})
+    c.should_receive(:new).with({"p" => "blah", "o" => "test"}).and_return(mock(c))
+    c.find_by_uri("http://test.host/")
+  end
+
+  it "should allow you to specify an id prefix" do
+    c = test_class
+    c.id_prefix "species"
+    c.id_prefix.should == "species"
+  end
+
+  it "should allow you to find by ID" do
+    c = test_class
+    @store.should_receive(:select).with("SELECT ?p ?o WHERE { species:1 ?p ?o }").and_return({"p" => "blah", "o" => "test"})
+    c.id_prefix "species"
+    c.should_receive(:new).with({"p" => "blah", "o" => "test"}).and_return(mock(c))
+    c.find_by_id(1)
   end
 end
