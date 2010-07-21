eadb739357d306c92b7b8d130e158993f4aee09e
David Workman <workmad3@gmail.com>
2010-07-21 16:47:09 +0100

    Storing attributes as an array now, so that multiple predicates of the same name don't get lost

--- spec/rdf_model/attributes_spec.rb
+++ spec/rdf_model/attributes_spec.rb
@@ -13,12 +13,17 @@ describe RdfModel::Attributes do
 
   it "should allow access to defined attributes" do
     c = test_class
-    c.new("", [{"p" => "testing", "o" => "test1"}]).testing.should == "test1"
+    c.new("", [{"p" => "testing", "o" => "test1"}]).testing.should == ["test1"]
   end
 
-  it "should populate the attributes hash with data" do
+  it "should populate the attributes hash with an array containing the attribute data" do
     c = test_class
-    c.new("", [{"p" => "testing", "o" => "test1"}]).attributes.should include({"testing" => "test1"})
+    c.new("", [{"p" => "testing", "o" => "test1"}]).attributes.should include({"testing" => ["test1"]})
+  end
+
+  it "should create an array for the data if an attribute appears multiple times" do
+    c = test_class
+    c.new("", [{"p" => "testing", "o" => "test1"}, {"p" => "testing", "o" => "test2"}]).attributes.should include({"testing" => ["test1", "test2"]})
   end
 
 end
